7b14aa0c688604baaae8233dbe7c068c96c19527
chikwong <527439024@qq.com>
2024-06-19 17:30:59 +0800

    fixed bug

--- snow_park/get_data.py
+++ snow_park/get_data.py
@@ -20,7 +20,7 @@ def get_customers():
     df_customer = session.sql(
         """
                               select
-                              distinct 
+                              distinct
                               sale.customer,
                               person.description,
                               sale.gc_channel
@@ -61,15 +61,15 @@ def convert_date(date_str):
         return None
     else:
         # print(date_str)
-        date_str_format = date_str[0:4] + '-' + date_str[4:6] + '-' + date_str[6:8]
-        return datetime.datetime.strptime(date_str_format, '%Y-%m-%d').date()
+        date_str_format = date_str[0:4] + "-" + date_str[4:6] + "-" + date_str[6:8]
+        return datetime.strptime(date_str_format, "%Y-%m-%d").date()
 
 
 def get_ar_open_document(date):
     session = create_session()
     df_1 = session.sql(
         """
-        select * from ods.fico.ods_v_ar_document 
+        select * from ods.fico.ods_v_ar_document
         where comp_code in ('HH41','HH31')
         and fi_docstat = 'O'
         and to_date(
@@ -79,7 +79,7 @@ def get_ar_open_document(date):
                     substr(pstng_date, 5, 2),
                     '-',
                     substr(pstng_date, 7, 2)
-                ) 
+                )
         ) <=  to_date('{p_date}')
         """.format(
             p_date=date
@@ -88,7 +88,7 @@ def get_ar_open_document(date):
 
     df_2 = session.sql(
         """
-            select * from ods.fico.ods_v_ar_document 
+            select * from ods.fico.ods_v_ar_document
             where comp_code in ('HH41','HH31')
             and to_date(
                 concat(
@@ -115,7 +115,7 @@ def get_ar_open_document(date):
     ).to_pandas()
     session.close()
     result = pd.concat([df_1, df_2])
-    result['DEBITOR'] = result['DEBITOR'].apply(lambda x: x.lstrip('0'))
+    result["DEBITOR"] = result["DEBITOR"].apply(lambda x: x.lstrip("0"))
     return result
 
 
@@ -130,28 +130,75 @@ def get_ar_open_document(date):
 #         else:
 #             row['LAST_POD_DATE'] = row['LAST_POD_DATE'] + timedelta(days=row['ZTAG1'])
 #             row['LAST_POD_DATE'] = row['LAST_POD_DATE'].date()
-def calculate_payment_day(row, docs):
-    if row['AC_DOC_TYP'] == 'KD':
-        return row['POSTING_DATE'] if row['POSTING_DATE'] > row['BASELINE_DATE'] else row['BASELINE_DATE']
+def calculate_payment_day(row):
+    print(row)
+    if row["AC_DOC_TYP"] == "KD":
+        return (
+            row["POSTING_DATE"]
+            if row["POSTING_DATE"] > row["BASELINE_DATE"]
+            else row["BASELINE_DATE"]
+        )
+    if (row["AC_DOC_TYP"] == "DA" or row["AC_DOC_TYP"] == "RV") and (
+        re.match("4800", row["ALLOC_NMBR"]) or re.match("3800", row["ALLOC_NMBR"])
+    ):
+        if re.match("G", row["PMNTTRMS"]):
+            return row["POD_DATE"] + timedelta(days=row["ZTAG1"])
+        else:
+            return row["LAST_POD_DATE"] + timedelta(days=row["ZTAG1"])
 
 
 if __name__ == "__main__":
-    df_open_docs = get_ar_open_document('2024-04-30')
+    df_open_docs = get_ar_open_document("2024-04-30")
     df_customers = get_customers()
     df_pod = get_pod()
-    result = df_open_docs.merge(df_customers, left_on='DEBITOR', right_on='CUSTOMER', how='inner',
-                                suffixes=(False, False))
-    result = result.merge(df_pod, left_on='ALLOC_NMBR', right_on='VBELN', how='left', suffixes=(False, False))
-    result['GJAHR'] = '2024-04-30'[:4]
-    result['POSTING_DATE'] = result['PSTNG_DATE'].apply(convert_date)
-    result['BASELINE_DATE'] = result['BLINE_DATE'].apply(convert_date)
-    result['POD_DATE'] = result['PODAT'].apply(convert_date)
-    result['LAST_POD_DATE'] = pd.to_datetime(result['POD_DATE'], format='%Y%m') + MonthEnd(1)
+    result = df_open_docs.merge(
+        df_customers,
+        left_on="DEBITOR",
+        right_on="CUSTOMER",
+        how="inner",
+        suffixes=(False, False),
+    )
+    result = result.merge(
+        df_pod,
+        left_on="ALLOC_NMBR",
+        right_on="VBELN",
+        how="left",
+        suffixes=(False, False),
+    )
+    result["GJAHR"] = "2024-04-30"[:4]
+    result["POSTING_DATE"] = result["PSTNG_DATE"].apply(convert_date)
+    result["BASELINE_DATE"] = result["BLINE_DATE"].apply(convert_date)
+    result["POD_DATE"] = result["PODAT"].apply(convert_date)
+    result["LAST_POD_DATE"] = pd.to_datetime(
+        result["POD_DATE"], format="%Y%m"
+    ) + MonthEnd(1)
     result = result.filter(
-        items=['COMP_CODE', 'DEBITOR', 'FISCPER', 'GJAHR', 'CUSTOMER_TYPE', 'GC_CHANNEL', 'AC_DOC_NO', 'ITEM_NUM',
-               'DEB_CRE_LC', 'ZTAG1', 'POSTING_DATE',
-               'BASELINE_DATE', 'POD_DATE', 'LAST_POD_DATE', 'REF_DOC_NO', 'PMNTTRMS', 'ALLOC_NMBR', 'AC_DOC_TYP'])
+        items=[
+            "COMP_CODE",
+            "DEBITOR",
+            "FISCPER",
+            "GJAHR",
+            "CUSTOMER_TYPE",
+            "GC_CHANNEL",
+            "AC_DOC_NO",
+            "ITEM_NUM",
+            "DEB_CRE_LC",
+            "ZTAG1",
+            "POSTING_DATE",
+            "BASELINE_DATE",
+            "POD_DATE",
+            "LAST_POD_DATE",
+            "REF_DOC_NO",
+            "PMNTTRMS",
+            "ALLOC_NMBR",
+            "AC_DOC_TYP",
+        ]
+    )
+    # print(result)
+    print(result[result.AC_DOC_TYP == "KD"])
     kd = result.query('AC_DOC_TYP == "KD"')
-    kd['PAYMENT_DATE'] = kd.apply(calculate_payment_day(result))
+    kd["PAYMENT_DATE"] = kd.apply(calculate_payment_day)
     da_rv = result.query('AC_DOC_TYP == "DA" or AC_DOC_TYP == "RV"')
+    da_rv["PAYMENT_DATE"] = da_rv.apply(calculate_payment_day)
+    print(kd)
     # print(result)
