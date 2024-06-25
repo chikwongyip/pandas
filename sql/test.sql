dd79fc9ab523748c70dd573a400c74f3db5834cb
chikwong <527439024@qq.com>
2024-06-25 17:35:16 +0800

    test

--- sql/test.sql
+++ sql/test.sql
@@ -11,3 +11,11 @@ select
 select * from dim.dim.dim1_t_customer_sales limit 100;
  select *
                     from darliesf1.sapabap1.t9pod where erdat >= '20230101' limit 100;
+
+
+select * from dwd.ecom.dwd_t_ecom_order_getlist_items limit 100;
+select * from dwd.ecom.dwd_t_ecom_order_getlist limit 100;
+
+select * from dwd.ecom.dwd_t_ecom_order_getlist_objects limit 100;
+
+select * from dwd.ecom.dwd_t_ecom_order_getlist_pmts limit 100;
